2dfdac580a69012ef2e9a1e291a83a4be25e7a66
noahthomlison <noahthomlison@gmail.com>
2022-03-10 08:41:01 -0800

    adds abilitly to ignore symbols and spaces and numbers, adds ability to shift properly with higher numbers (greater than 26)

--- day-8/caesarCipher.py
+++ day-8/caesarCipher.py
@@ -1,29 +1,50 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def caesar (text, shift, direction):
   shiftedMessage = ""
-  resetAmount = 26
+  if(shift > 26):
+    shift = shift%26
   if(direction == "encode"):
     shift *= 1
-    resetAmount *= -1
   if(direction == "decode"):
     shift *= -1
-    resetAmount *= 1
-
+  print(shift)
   for letter in text:
-    if alphabet.index(letter)+shift < len(alphabet):
-      shiftedLetter = alphabet[alphabet.index(letter)+shift]
-      shiftedMessage += shiftedLetter
+    if letter in alphabet:
+      if alphabet.index(letter)+shift < len(alphabet):
+        shiftedLetter = alphabet[alphabet.index(letter)+shift]
+        shiftedMessage += shiftedLetter
+      else:
+        shiftedLetter = alphabet[alphabet.index(letter)+shift]
+        shiftedMessage += shiftedLetter
     else:
-      shiftedLetter = alphabet[alphabet.index(letter)+shift+resetAmount]
-      shiftedMessage += shiftedLetter
+      shiftedMessage += letter
   return(shiftedMessage)
 
-encodedMessage = (caesar(text, shift, direction))
-dencodedMessage = (caesar(encodedMessage, shift, direction))
-print(encodedMessage)
+encodedMessage = (caesar(text, shift, "encode"))
+dencodedMessage = (caesar(encodedMessage, shift, "decode"))
+print(encodedMessage) 
 print(dencodedMessage)
