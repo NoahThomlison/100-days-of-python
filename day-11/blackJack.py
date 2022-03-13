ceb0a1ae585ab1d7f54e7a1137db390336820b68
noahthomlison <noahthomlison@gmail.com>
2022-03-12 17:16:15 -0800

    adds functionailty to check win and gameplay. Completes blackjack game.

--- day-11/blackJack.py
+++ day-11/blackJack.py
@@ -1,22 +1,3 @@
-  ############### Blackjack Project #####################
-
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
 logo = """
 .------.            _     _            _    _            _    
 |A_  _ |.          | |   | |          | |  (_)          | |   
@@ -41,7 +22,6 @@ visibleDealerCards = []
 playerCards = []
 
 def dealCards(player):
-  # print(len(cards))
   dealtCard = cards[random.randint(0, len(cards)-1)]
   if(player == "dealer" and len(dealerCards) == 0):
     dealerCards.append(dealtCard)
@@ -51,66 +31,61 @@ def dealCards(player):
   else:
     playerCards.append(dealtCard)
   cards.remove(dealtCard)
-  # print(len(cards))
-  # print(dealtCard)
   return player
 
+def displayCards ():
+  print("something")
+
+def checkScore (cards):
+  score = 0
+  for card in cards:
+    if(card == "Q" or card == "J" or card == "K" or card == "A"):
+      score += 10
+    elif(card == "A" and score <= 11):
+      score += 10
+    else:
+      score += int(card)
+  if("A" in cards and score > 21):
+    score = score - 10 + 1
+  return score
+
+def initialDeal():
+  dealCards("dealer")
+  dealCards("dealer")
+  dealCards("player")
+  dealCards("player")
+
+
 def gamePlay ():
-  activePlayer = "dealer"
-  dealCards(activePlayer)
-  dealCards(activePlayer)
+  hitOrStand = "h"
+  playerScore = checkScore(playerCards)
+  dealerScore = checkScore(dealerCards)
+  bust = False
+  initialDeal()
 
-  activePlayer = "player"
-  dealCards(activePlayer)
-  dealCards(activePlayer)
+  print(f"Dealer Cards: {visibleDealerCards}")
+  print(f"Player Cards: {playerCards}")
 
-  print(dealerCards)
-  print(visibleDealerCards)
-  print(playerCards)
+  while hitOrStand == "h" and playerScore < 21:
+    hitOrStand = input("Hit or Stand? ")
+    hitOrStand = hitOrStand.lower()
+    if hitOrStand == "hit" or hitOrStand == "h":
+      dealCards("player")
+      print(f"Player Cards: {playerCards} and Player Score: {checkScore(playerCards)}")
+      playerScore = checkScore(playerCards)
+    if(playerScore > 21):
+      print("Player Busts")
+      bust = True
+
+  while (dealerScore) < checkScore(playerCards) and dealerScore < 22 and playerScore < 22 and bust != True:
+    dealCards("dealer")
+    dealerScore = checkScore(dealerCards)
+
+  print(f"Player Cards: {playerCards} and Player Score: {checkScore(playerCards)}")
+  print(f"Dealer Cards: {dealerCards} and Dealer Score: {checkScore(dealerCards)}")
+  if(dealerScore > playerScore and dealerScore < 22 and playerScore < 22 or bust):
+    print("Dealer Wins")
+  else:
+    print("Player Wins")
 
 gamePlay()
-
-
-
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
-
-#Hint 2: Read this breakdown of program requirements: 
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created: 
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
-
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
-
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
-
