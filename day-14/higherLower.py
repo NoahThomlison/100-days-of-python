192d195c673a264f697065658282898b264f4346
noahthomlison <noahthomlison@gmail.com>
2022-03-15 20:13:59 -0700

    initial commit of day14 work, higherOrLower.py

---
+++ day-14/higherLower.py
@@ -0,0 +1,33 @@
+logo = """
+    __  ___       __             
+   / / / (_)___ _/ /_  ___  _____
+  / /_/ / / __ `/ __ \/ _ \/ ___/
+ / __  / / /_/ / / / /  __/ /    
+/_/ ///_/\__, /_/ /_/\___/_/     
+   / /  /____/_      _____  _____
+  / /   / __ \ | /| / / _ \/ ___/
+ / /___/ /_/ / |/ |/ /  __/ /    
+/_____/\____/|__/|__/\___/_/     
+"""
+
+vs = """
+ _    __    
+| |  / /____
+| | / / ___/
+| |/ (__  ) 
+|___/____(_)
+"""
+
+
+from data import *
+import random
+
+print (data)
+
+def game ():
+  firstComparisionIndex = random.randint(0, len(data)-1)
+  secondComparisionIndex = random.randint(0, len(data)-1)
+  print(firstComparisionIndex)
+  print(secondComparisionIndex)
+
+game()
