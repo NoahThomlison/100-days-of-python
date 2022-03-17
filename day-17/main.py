9c37f52254f0201ad195018ec26eaf4c6593f16a
noahthomlison <noahthomlison@gmail.com>
2022-03-17 16:43:51 -0700

    adds completed code for quiz game

--- day-17/main.py
+++ day-17/main.py
@@ -1,8 +1,16 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 questionBank = []
 for question in question_data:
   newQuestion = (Question(question['text'], question['answer']))
   questionBank.append(newQuestion)
-print(questionBank)
+
+quiz = QuizBrain(questionBank)
+quiz.next_question()
+
+while(quiz.more_questions()):
+  quiz.next_question()
+
+print(f"You have completed the quiz game. Your final score was {quiz.score}/{quiz.question_number}")
