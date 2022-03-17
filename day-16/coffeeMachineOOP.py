c546ce84d2eb90651aa33a1ec36391025eb3a76b
noahthomlison <noahthomlison@gmail.com>
2022-03-17 14:34:51 -0700

    adds OOP version of coffeeMachine which relies on given classes for the menu, the coffee machine and the money processing machine

--- day-16/coffeeMachineOOP.py
+++ day-16/coffeeMachineOOP.py
@@ -1,3 +1,28 @@
+from pickle import TRUE
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+def runMachine():
+  coffeeMachine = CoffeeMaker()
+  moneyMachine = MoneyMachine()
+  coffeeMenu = Menu()
+  running = True
+
+  while running == True:
+    drinkOrder = input(f"What would you like to order? ({coffeeMenu.get_items()}) ")
+    if(drinkOrder == "off"):
+      running = False
+    elif(drinkOrder == "report"):
+      coffeeMachine.report()
+      moneyMachine.report()
+    else:
+      drink = coffeeMenu.find_drink(drinkOrder)
+      if(coffeeMachine.is_resource_sufficient(drink)):
+        print(f"Your total is {drink.cost}")
+        moneyPaid = moneyMachine.make_payment(drink.cost)
+        if(moneyPaid):
+          coffeeMachine.make_coffee(drink)
+
+runMachine()
