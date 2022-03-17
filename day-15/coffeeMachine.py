4f6a7df4fdcaebedefdd7a82e151291ad3d5cbd8
noahthomlison <noahthomlison@gmail.com>
2022-03-17 10:47:24 -0700

    adds a takePayment function which takes payment for drink, checks if it is enough, returns fasle if not enough, gives change, and updates value of resources to add the value of the drink to the machine balace

--- day-15/coffeeMachine.py
+++ day-15/coffeeMachine.py
@@ -1,3 +1,6 @@
+from functools import total_ordering
+
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -28,29 +31,46 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "coins" : 0,
 }
 
 def checkResources(order):
   haveIngredient = True
   for ingredient in MENU[order]["ingredients"]:
-    print(MENU[order]["ingredients"][ingredient])
-    print(resources[ingredient])
+    # print(MENU[order]["ingredients"][ingredient])
+    # print(resources[ingredient])
     if((MENU[order]["ingredients"][ingredient]) > (resources[ingredient])):
       print(f"You do not have enough {ingredient}. Please refill the machine")
       haveIngredient = False
   return(haveIngredient)
 
 
-def takePayment():
-  print("nothing")
+def takePayment(order):
+  print(f"Your order of {order} costs {MENU[order]['cost']}")
+  quarters = int(input("How many quarters? "))
+  dimes = int(input("How many dimes? "))
+  nickles = int(input("How many nickles? "))
+  pennies = int(input("How many pennies? "))
+  totalPayment = quarters * .25 + dimes * .1 + nickles * .05 + pennies * .01
+  
+  if totalPayment < MENU[order]['cost']:
+    print("You have not put in enough money. Please start over. Refunding deposited money")
+    return(False)
+  else:
+    change = MENU[order]['cost'] - totalPayment
+    print(f"Your change is {change}")
+    resources["coins"] = MENU[order]['cost']
+
+  print(resources["coins"])
 
 def updateResources(drinkOrder):
   print("nothing")
 
 def interface (MENU, resources):
   drinkOrder = input("What would you like to order? (espresso/latte/cappuccino) ")
-  checkResources(drinkOrder)
-  takePayment()
+  if checkResources(drinkOrder):
+    takePayment(drinkOrder)
+
   updateResources(drinkOrder)
 
 interface(MENU, resources)
