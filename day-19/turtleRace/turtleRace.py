4e55ba468b94a9e95f7f468b0b4b700d8aa88ab8
noahthomlison <noahthomlison@gmail.com>
2022-03-19 16:35:31 -0700

    adds complete turtle race code

--- day-19/turtleRace/turtleRace.py
+++ day-19/turtleRace/turtleRace.py
@@ -1,7 +1,40 @@
+from operator import truediv
+from pickle import TRUE
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet?", prompt="Who will win?")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+is_race_on = TRUE
+turtles = []
+x = -230
+y = 100
+for color in colors:
+  print(y)
+  print(color)
+  turtleName = color + "turtle"
+  turtleName = Turtle(shape="turtle")
+  turtleName.penup()
+  turtleName.color(color)
+  turtleName.goto(x, y)
+  y -= 40
+  turtles.append(turtleName)
 
+print(turtles)
 
+while (is_race_on):
+  for turtle in turtles:
+    rand_distance = random.randint(0, 10)
+    turtle.forward(rand_distance)
+    print(turtle.xcor())
+    if(turtle.xcor() >= 230):
+      is_race_on = False
+      winningTurtle = (turtle.pencolor())
+      if(winningTurtle == user_bet):
+        print(f"You won! The {winningTurtle} won!")
+      else:
+        print(f"You lose, the winning turtle was {winningTurtle}")
+        
 screen.exitonclick()
