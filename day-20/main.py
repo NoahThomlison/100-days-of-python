807db5391250169bbbf2702c4a191915a4c6b7f8
noahthomlison <noahthomlison@gmail.com>
2022-03-22 07:42:12 -0700

    refactors collision detection

--- day-20/main.py
+++ day-20/main.py
@@ -18,17 +18,18 @@ foodPresent = False
 snake = Snake()
 food = Food()
 array = []
-print(snake)
 while gameOn:
   screen.onkey(key="a", fun=snake.turn_left)
   screen.onkey(key="d", fun=snake.turn_right)
   screen.update()
   time.sleep(.25)
+
+  if snake.head.distance(food) < 15:
+      food.refresh()
+      snake.eatFood()
+  
   snake.move()
 
-  if(snake.positions[0] == food.position):
-    food.clear_food()
-    food.createFood()
 
 gameOn = True
 
