3d3a07c90a0cdc51ab80627991109c21cc4ed3b9
noahthomlison <noahthomlison@gmail.com>
2022-03-22 07:43:21 -0700

    adds eatFood method and head attitube

--- day-20/snake.py
+++ day-20/snake.py
@@ -2,9 +2,10 @@ from turtle import Turtle
 
 class Snake():
   def __init__(self) -> None:
-      self.positions = [(0,0), (-20, 0), (-40, 0)]
-      self.snakeSegments = []
-      self.createSegment(3)
+    self.positions = [(0,0), (-20, 0), (-40, 0)]
+    self.snakeSegments = []
+    self.createSegment(3)
+    self.head = self.snakeSegments[0]
 
   def createSegment(self, numberOfSegments):
     for xPos in range (0, numberOfSegments):
@@ -13,6 +14,15 @@ class Snake():
       snakeBody.penup()
       snakeBody.goto(self.positions[xPos])
       self.snakeSegments.append(snakeBody)
+
+  def eatFood(self):
+    snakeBody = Turtle("square")
+    snakeBody.color("white")
+    snakeBody.penup()
+    position = self.positions[len(self.positions)-1]
+    snakeBody.goto(position)
+    self.positions.append(position)
+    self.snakeSegments.append(snakeBody)
 
   def turn_left(self):
     self.snakeSegments[0].left(90)
@@ -23,14 +33,10 @@ class Snake():
   def move(self):
     for index in range(len(self.snakeSegments)-1, 0, -1):
       currentHead = self.positions[index-1]
-      print(currentHead)
       self.snakeSegments[index].goto(currentHead)
       self.positions[index] = currentHead
     self.snakeSegments[0].forward(20)
     self.positions[0] = self.snakeSegments[0].pos()
 
-
-    # currentHead = self.positions[index-1]
-
-  def printLocations(self):
-    print(self.positions)
+  def getLocations(self):
+    return(self.positions)
