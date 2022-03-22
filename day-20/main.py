886d4a46d1cdeb639b09611aa72841db24850438
noahthomlison <noahthomlison@gmail.com>
2022-03-22 07:47:32 -0700

    adds collisions with snake and wall

--- day-20/main.py
+++ day-20/main.py
@@ -24,10 +24,25 @@ while gameOn:
   screen.update()
   time.sleep(.25)
 
+  #Detect collision with food.
   if snake.head.distance(food) < 15:
       food.refresh()
       snake.eatFood()
-  
+
+  #Detect collision with wall.
+  if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+      game_is_on = False
+      
+  #Detect collision with tail.
+  for segment in snake.snakeSegments:
+      print(segment)
+      print(snake.head.distance(segment))
+
+      if segment == snake.head:
+          pass
+      elif snake.head.distance(segment) < 10:
+          gameOn = False
+
   snake.move()
 
 
