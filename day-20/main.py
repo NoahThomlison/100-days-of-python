40e19b91da12518cca4fc92a0f69f58df8b9ce3a
noahthomlison <noahthomlison@gmail.com>
2022-03-24 11:43:22 -0700

    adds scoredboard to game

--- day-20/main.py
+++ day-20/main.py
@@ -5,6 +5,7 @@ import random
 import time
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -17,6 +18,8 @@ gameOn = True
 foodPresent = False
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
+
 array = []
 while gameOn:
   screen.onkey(key="a", fun=snake.turn_left)
@@ -26,19 +29,22 @@ while gameOn:
 
   #Detect collision with food.
   if snake.head.distance(food) < 15:
-      food.refresh()
-      snake.eatFood()
+    food.refresh()
+    snake.eatFood()
+    scoreboard.increase_score()
 
   #Detect collision with wall.
   if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-      game_is_on = False
-      
+    game_is_on = False
+    scoreboard.game_over()
+
   #Detect collision with tail.
   for segment in snake.snakeSegments:
       if segment == snake.head:
-          pass
+        pass
       elif snake.head.distance(segment) < 10:
-          gameOn = False
+        gameOn = False
+        scoreboard.game_over()
 
   snake.move()
 
