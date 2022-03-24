5c8bcfc77f5e90708189530040b2c5909b8bfefb
noahthomlison <noahthomlison@gmail.com>
2022-03-24 12:28:01 -0700

    adds finished code for letter writter which files in name on letter template from a file of names and outputs a series of personalized letters

--- day-24/main.py
+++ day-24/main.py
@@ -1,9 +1,14 @@
-with open("./input/Names/invited_names.txt") as nameFiles:
-  names = nameFiles.read()
+from fileinput import filename
 
+with open("./input/Names/invited_names.txt") as nameFiles:
+  names = (nameFiles.read())
+  namesArray = names.split('\n')
 with open("./input/Letters/starting_letter.text") as letter:
   letterTemplate = letter.read()
 
-print(names)
-print(letterTemplate)
-
+for name in namesArray:
+  print (name)
+  fileName = f"letter_for_{name}"
+  with open(f"./output/readyToSend/{fileName}.txt", "w") as file:
+    letter = letterTemplate.replace("[name]", f"{name}")
+    file.write(letter)
