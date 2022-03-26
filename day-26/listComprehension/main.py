4dfe465582e44524304e929730a01f1923eedaa8
noahthomlison <noahthomlison@gmail.com>
2022-03-25 22:09:04 -0700

    adds functional code to compare common numbers between two sets of imported lists

---
+++ day-26/listComprehension/main.py
@@ -0,0 +1,8 @@
+with open("file1.txt") as file1:
+  file1Data = file1.readlines()
+with open("file2.txt") as file2:
+  file2Data = file2.readlines()
+
+  commonNumbers = [int(num) for num in file1Data if num in file2Data]
+  print(commonNumbers)
+# commonData = [data in data for file1Data if ]
