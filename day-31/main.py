62df8f18fadab264983683a41085ca2808adcf87
noahthomlison <noahthomlison@gmail.com>
2022-03-30 07:15:23 -0700

    imports data from csv and displays on button click, refactors ui

--- day-31/main.py
+++ day-31/main.py
@@ -1,5 +1,15 @@
 BACKGROUND_COLOR = "#B1DDC6"
 from tkinter import *
+import pandas
+import random
+words = pandas.read_csv("data/french_words.csv")
+wordsDict = {row["French"]: row["English"] for (index, row) in words.iterrows()}
+
+# Change Word
+def changeWord():
+  englishWord, frenchWord = random.choice(list(wordsDict.items()))
+  canvas.itemconfig(card_word, text=frenchWord)
+  canvas.itemconfig(card_title, text="French")
 
 # Create UI
 window = Tk()
@@ -9,23 +19,18 @@ cardFront = PhotoImage(file="images/card_front.png")
 cardBack = PhotoImage(file="images/card_back.png")
 window.title("Flashy")
 window.config(padx=50, pady=50, width=1000, height=800, bg=BACKGROUND_COLOR)
-background_image=PhotoImage(cardFront)
+
 canvas = Canvas( bg=BACKGROUND_COLOR, width=800, height=526, highlightthickness=0)
-canvas.place(x=0, y=0)
+card_background = canvas.create_image(400, 263, image=cardFront)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
 
-check_button = Button(image=rightImage, highlightthickness=0)
-check_button.place(x=600, y=600)
-x_button = Button(image=wrongImage, highlightthickness=0)
-x_button.place(x=200, y=600)
-
-cardFrontLabel = Label(width=800, height=526, image=cardBack, highlightthickness=0, bg=BACKGROUND_COLOR)
-cardFrontLabel.place(x=50, y=50)
-
-cardLabel = Label(text="French", highlightthickness=0, font=("Ariel", 40, "italic"), bg=BACKGROUND_COLOR, anchor="center")
-cardWord = Label(text="Trouve", font=("Ariel", 60, "bold"), bg=BACKGROUND_COLOR, anchor="center")
-cardLabel.place(x=375, y=150)
-cardWord.place(x=325, y=263)
-
-
+#Buttons
+check_button = Button(image=rightImage, highlightthickness=0, command=changeWord)
+check_button.grid(row=2, column=0)
+x_button = Button(image=wrongImage, highlightthickness=0, command=changeWord)
+x_button.grid(row=2, column=1)
 
 window.mainloop()
+
