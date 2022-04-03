caac65525bcf752b24bc92d3da148a182c737fc0
noahthomlison <noahthomlison@gmail.com>
2022-04-02 18:52:43 -0700

    adds completed code

---
+++ day-35/rainAlert.py
@@ -0,0 +1,41 @@
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = "YOUR ACCOUNT SID"
+auth_token = os.environ.get("AUTH_TOKEN")
+
+parameters = {
+  "appid":'ab89fe26fedab37c57a9d332d8f61b27', 
+  "lat": "49.2827", 
+  "lon": "-123.1207",
+  "exclude": "current, daily, minutely"
+  }
+
+response = requests.get(OWM_Endpoint, params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="YOUR TWILIO VIRTUAL NUMBER",
+        to="YOUR TWILIO VERIFIED REAL NUMBER"
+    )
+    print(message.status)
