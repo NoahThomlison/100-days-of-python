ba5d9cae2d586c45104ab5ceafdad9e06f2db930
noahthomlison <noahthomlison@gmail.com>
2022-04-12 21:29:40 -0700

    git adds code to get top 100 songs from inputted date

--- day-46/top100songs.py
+++ day-46/top100songs.py
@@ -1,13 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-date = input("What year would you like to travel to? YYYY-MM-DD ")
+date = input("What year would you like to travel to? (YYYY-MM-DD) ")
 URL = f"https://www.billboard.com/charts/hot-100/{date}/"
 
 response = requests.get(URL)
 siteInfo = BeautifulSoup(response.text, "html.parser")
 
-songs = siteInfo.findAll(class_="c-title__link")
-print(songs)
+songs = siteInfo.findAll("h3", class_="c-title", id="title-of-a-story")
+songTitles = []
 for song in songs:
-  print(song)
+  songTitle = song.getText().strip("\n")
+  songTitle = songTitle.replace('\r', '').replace('\n', '').replace('\t', "")
+  if(songTitle != "Songwriter(s):" and songTitle != "Producer(s):" and songTitle != "Imprint/Promotion Label:"):
+    songTitles.append(songTitle)
+
+print(songTitles)
