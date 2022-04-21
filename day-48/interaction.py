e34f7b6c9c03b3684b5391b2a5e52fd2e113bef8
noahthomlison <noahthomlison@gmail.com>
2022-04-20 21:28:23 -0700

    auto inputs text and searchs for python

--- day-48/interaction.py
+++ day-48/interaction.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
 chrome_driver_path = r"C:\Users\User\Documents\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
-articleCount = driver.find_element_by_xpath('//*[@id="articlecount"]/a[1]').text
-print(articleCount)
+articleCount = driver.find_element_by_xpath('//*[@id="articlecount"]/a[1]')
+# articleCount.click()
+
+search = driver.find_element_by_name("search")
+search.send_keys("python")
+search.send_keys(Keys.ENTER)
+
+# searchGo = driver.find_element_by_id("searchButton").click()
