f7527fd94dfbe39caf7de8ff4506d369749535bf
noahthomlison <noahthomlison@gmail.com>
2022-04-21 08:01:51 -0700

    adds basic cookie playing bot

--- day-48/cookieClicker.py
+++ day-48/cookieClicker.py
@@ -6,30 +6,25 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 cookie = driver.find_element_by_id("bigCookie")
 score = driver.find_element_by_id("cookies")
+
+items = driver.find_elements_by_css_selector("#store div")
+
+
 cursorUpgrade = driver.find_element_by_xpath('//*[@id="product0"]')
+cursorPrice = driver.find_element_by_id("productPrice0").text
+
 grandmaUpgrade = driver.find_element_by_xpath('//*[@id="product1"]')
-# farmUpgrade = driver.find_element_by_xpath('//*[@id="product2"]')
-# mineUpgrade = driver.find_element_by_xpath('//*[@id="product3"]')
-# upgrade1 = driver.find_element_by_xpath('//*[@id="upgrade0"]')
-# upgrade2 = driver.find_element_by_xpath('//*[@id="upgrade1"]')
-# upgrade3 = driver.find_element_by_xpath('//*[@id="upgrade2"]')
-# upgrade4 = driver.find_element_by_xpath('//*[@id="upgrade3"]')
-# upgrade5 = driver.find_element_by_xpath('//*[@id="upgrade4"]')
+grandmaPrice = driver.find_element_by_id('productPrice1').text
 
 for i in range(10000):
   scoreNumber = (score.text.split(" "))
-  # upgrade1.click()
-  # upgrade2.click()
-  # upgrade3.click()
-  # upgrade4.click()
-  # upgrade5.click()
-  cursorUpgrade.click()
-  grandmaUpgrade.click()
-  # farmUpgrade.click()
-  # mineUpgrade.click()
-
-  # if(scoreNumber[0] > 15)
-  # if int(score.text) > 15:
-  #   cursorUpgrade.click()
+  scoreNumber = int(scoreNumber[0])
+  if(scoreNumber > int(cursorPrice)):
+    cursorUpgrade.click()
+    cursorPrice = driver.find_element_by_id("productPrice0").text
+  if(scoreNumber > int(grandmaPrice)):
+    grandmaUpgrade.click()
+    grandmaPrice = driver.find_element_by_id('productPrice1').text
+  print(scoreNumber)
   cookie.click()
 
