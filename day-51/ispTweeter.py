5be98590caa97e7d16abed9ec1dd1892aca9e3e0
noahthomlison <noahthomlison@gmail.com>
2022-04-21 18:15:46 -0700

    adds completed isp twitter bot

--- day-51/ispTweeter.py
+++ day-51/ispTweeter.py
@@ -17,28 +17,48 @@ class InternetSpeedTwitterBot ():
     self.driver = driver
     self.up = 0
     self.down = 0
-    
+    self.promisedUp = upSpeed
+    self.promisedDown = downSpeed
 
   def get_internet_speed(self):
-    driver.get('https://www.speedtest.net/')
+    self.driver.get('https://www.speedtest.net/')
     button = driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
     button.click()
     time.sleep(45)
     self.up = driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
     self.down = driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
     print(f'up: {self.up}, down: {self.down}')
-
+    if(self.up < self.promisedUp or self.down < self.promisedDown):
+      self.tweet_at_provider()
     return(self.up, self.down)
 
   def tweet_at_provider(self):
+    self.driver.get("https://twitter.com/login")
+    time.sleep(5)
+    username = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div[1]/div/div/div[5]/label/div/div[2]/div/input')
+    username.send_keys(twitterEmail)
+    username.send_keys(Keys.ENTER)
 
+    time.sleep(5)
+
+    password = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div[1]/div/div/div[5]/label/div/div[2]/div/input')
+    password.send_keys(twitterPassword)
+    password.send_keys(Keys.ENTER)
     print("Test")
 
+    time.sleep(5)
 
+    tweet = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+    tweetMessage = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {upSpeed}down/{downSpeed}up?"
+    tweet.send_keys(tweetMessage)
+
+    time.sleep(3)
+
+    tweet_button = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+    tweet_button.click()
 
 bot = InternetSpeedTwitterBot(driver, upSpeed, downSpeed)
-# bot.get_internet_speed()
-bot.tweet_at_provider()
+bot.get_internet_speed()
     
     
 
