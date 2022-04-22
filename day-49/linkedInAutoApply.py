fefba2805331ace8be02dda2eb5a62747e58498b
noahthomlison <noahthomlison@gmail.com>
2022-04-22 10:56:09 -0700

    adds multiple job search types, adds quick search vs complex search pathways

--- day-49/linkedInAutoApply.py
+++ day-49/linkedInAutoApply.py
@@ -2,42 +2,82 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 import time
+linkedInPassword = ""
+linkedInEmail = ""
 
 chrome_driver_path = r"C:\Users\User\Documents\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get('https://www.linkedin.com/jobs/search/?f_AL=true&f_E=1%2C2%2C3&f_WT=2&geoId=101174742&keywords=junior%20react%20developer&location=canada')
+jobType = int(input("What job would you like to apply for? Front end (1), full stack (2), or react (3) "))
+quickApplyOnly = input("Would you like to only apply to quick apply jobs? (y/n) ")
+
+if(jobType == 1):
+  # front end, junior, canada remote jobs
+  driver.get('https://www.linkedin.com/jobs/search/?f_AL=true&f_E=2&f_JT=F&f_T=9%2C3172%2C25170%2C25201&f_TPR=r2592000&f_WT=2&geoId=101174742&keywords=frontend%20developer&  location=Canada&sortBy=R')
+
+elif(jobType == 2):
+  # # full stack, junior, canada remote jobs
+  driver.get('https://www.linkedin.com/jobs/search/?f_AL=true&f_E=2&f_T=9%2C25170%2C3172%2C25201&f_TPR=r2592000&f_WT=2&geoId=101174742&keywords=full%20stack%20developer& location=Canada&sortBy=R')
+
+elif(jobType == 3):
+  # # react, junior, canada remote jobs
+  driver.get('https://www.linkedin.com/jobs/search/?f_AL=true&f_E=2&f_T=9%2C25170%2C3172%2C25201&f_TPR=r2592000&f_WT=2&geoId=101174742&keywords=react%20developer&  location=Canada&sortBy=R')
+
+if(quickApplyOnly == "y"):
+  quickApplyOnly == True
+else:
+  quickApplyOnly == False
 
 signIn = driver.find_element_by_xpath("/html/body/div[1]/header/nav/div/a[2]")
 signIn.click()
 email = driver.find_element_by_id("username")
-email.send_keys("")
+email.send_keys(linkedInEmail)
 
 password = driver.find_element_by_id("password")
-password.send_keys("")
+password.send_keys(linkedInPassword)
 password.send_keys(Keys.ENTER)
 
 jobs = driver.find_elements_by_class_name("jobs-search-results__list-item")
+try:
+  pages = driver.find_elements_by_css_selector('.artdeco-pagination__pages li')
+except NoSuchElementException:
+  pages = [0]
 
+for page in pages:
 for i in range(len(jobs)):
   jobs[i].click()
   time.sleep(1)
-  
   try:
+    driver.find_element_by_css_selector("[aria-label='Dismiss']")
     apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
     apply_button.click()
     time.sleep(1)
-
     submit_button = driver.find_element_by_css_selector("footer button")
     if submit_button.get_attribute("aria-label") == "Continue to next step":  
-      time.sleep(1)
-      driver.find_element_by_css_selector("[aria-label='Dismiss']").click()
-      time.sleep(1)
-      driver.find_element_by_css_selector("[data-control-name='discard_application_confirm_btn']").click()  
-      print("Complex application, skipped.")
-      continue
+      if quickApplyOnly:
+        time.sleep(1)
+        driver.find_element_by_css_selector("[aria-label='Dismiss']").click()
+        time.sleep(1)
+        driver.find_element_by_css_selector("[data-control-name='discard_application_confirm_btn']").click()  
+        print("Complex application, skipped.")
+        continue
+      else:
+        time.sleep(1)
+        submit_button.click() 
+        time.sleep(1)
+        submit_button.click() 
+        time.sleep(10)
+        driver.find_element_by_css_selector('[aria-label="Review your application"]').click()
+        time.sleep(1)
+        driver.find_element_by_css_selector('[aria-label="Submit application"]').click()
+        time.sleep(5)
     else:
+      print("Applied")
       submit_button.click()  
+      driver.find_element_by_css_selector('[aria-labelledby="post-apply-modal"] button').click()
+
   except NoSuchElementException:
     print("No application button, skipped.")
     continue
+  page.click()
+
