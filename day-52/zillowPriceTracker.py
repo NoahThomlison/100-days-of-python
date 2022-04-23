0a9bd0411c3f0f579951a3dc8ae7f849c349dd98
noahthomlison <noahthomlison@gmail.com>
2022-04-22 21:04:25 -0700

    adds webscrapper for zillow

---
+++ day-52/zillowPriceTracker.py
@@ -0,0 +1,37 @@
+from asyncio.windows_events import NULL
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+formLink = "https://docs.google.com/forms/d/e/1FAIpQLScBe395z0JkgrXxe46kDFEzLrhV8DzOKLmbYfDNvOMijiwMnw/viewform?usp=sf_link"
+zillowLink = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D"
+
+
+
+url = "https://www.amazon.com/Duo-Evo-Plus-esterilizadora-vaporizador/dp/B07W55DDFB/ref=sr_1_4?qid=1597660904"
+req_headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.8',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+}
+
+response = requests.get(zillowLink, headers=req_headers)
+soup = BeautifulSoup(response.content, "html.parser")
+
+listings = []
+listing = {
+  "link": NULL,
+  "price": NULL,
+  "address": NULL
+  }
+
+dataCards = soup.find_all(class_="list-card-info")
+for i in range(4):
+  link = dataCards[i].find(class_='list-card-link', href=True)
+  listing["link"] = link['href']
+  listing["price"] = dataCards[i].find(class_='list-card-price').text
+  listing["address"] = dataCards[i].find(class_='list-card-addr').text
+  listings.append(listing)
+  print(listings[i])
