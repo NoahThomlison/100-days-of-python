0f1fbaf22e2c525ee42350411f7f42fa4d582843
noahthomlison <noahthomlison@gmail.com>
2022-04-30 10:15:19 -0700

    refactors to use url_for, adds in posts

--- day-59/server.py
+++ day-59/server.py
@@ -2,11 +2,11 @@ from urllib import response
 from flask import Flask, render_template
 import requests
 app = Flask(__name__)
+url = "https://api.npoint.io/02d5e286e053bdcf0a39"
+posts = requests.get(url).json()
 
 @app.route('/')
 def home():
-    url = "https://api.npoint.io/02d5e286e053bdcf0a39"
-    posts = requests.get(url).json()
     return render_template("index.html", posts=posts)
 
 @app.route('/about')
@@ -19,13 +19,11 @@ def contact():
 
 @app.route("/post/<int:index>")
 def show_post(index):
-    url = "https://api.npoint.io/02d5e286e053bdcf0a39"
-    posts = requests.get(url).json()
     requested_post = None
     for blog_post in posts:
         if blog_post["id"] == index:
             requested_post = blog_post
-    return render_template("post.html", post=requested_post)
+    return render_template("post.html", post=requested_post, index=index)
 
 if __name__ == "__main__":
     app.run(debug=True)
