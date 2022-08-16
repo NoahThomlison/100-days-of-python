198ec0472708adba58e4f7c5eb688fe4c3d0c6b2
noahthomlison <noahthomlison@gmail.com>
2022-08-15 21:19:28 -0700

    add character input

---
+++ practiceProblems/Character Input.py
@@ -0,0 +1,9 @@
+# Character Input
+
+name = input('What is your name? ')
+age = int(input('What is your age? '))
+currentYear = 2022
+yearsTil100 = 100 - age
+oldYear = currentYear + yearsTil100
+
+print((f'Hello {name} you will be 100 years old in year {oldYear}'))
