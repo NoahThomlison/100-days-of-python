c61584d4baf6b7fea9288f148d3282940bf8c46f
noahthomlison <noahthomlison@gmail.com>
2022-08-17 21:26:25 -0700

    test

--- practiceProblems/String Lists.py
+++ practiceProblems/String Lists.py
@@ -1,10 +1,5 @@
-from audioop import reverse
 
 
-input = input("Give me word to check for palindrome? ")
-reverseInput = list(input)
-reverseInput.reverse()
-string = ""
 string = string.join(reverseInput)
 print(string)
 if(input == string):
